9bd3ab14a4bb6093a83c956cf298d9db8f8c102d
Aaradhya Gopal Nepal <aradhya.1221@gmail.com>
2026-03-24 10:40:33 +0545

    Generating pem key

--- API/simpleapi/go.mod
+++ API/simpleapi/go.mod
@@ -1,3 +1,8 @@
 module simpleapi
 
-go 1.24.5
+go 1.25.0
+
+require (
+	golang.org/x/net v0.52.0 // indirect
+	golang.org/x/text v0.35.0 // indirect
+)
--- API/simpleapi/server.go
+++ API/simpleapi/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+// To generate key: openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem
 func main() {
+	//http vs https
 	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Handling incoming orders")
 	})
@@ -14,5 +17,8 @@ func main() {
 	})
 	port := 3000
 	fmt.Println("Server is running on port:", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		log.Fatalln("Could not start server", err)
+	}
 }
