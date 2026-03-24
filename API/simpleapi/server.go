6bf3f60c6d1a594a16ffec426cd87e1f4541ff43
Aaradhya Gopal Nepal <aradhya.1221@gmail.com>
2026-03-24 12:16:28 +0545

    http2 practice

--- API/simpleapi/server.go
+++ API/simpleapi/server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 // To generate key: openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem
@@ -16,9 +19,26 @@ func main() {
 		fmt.Fprintf(w, "Handling incoming users")
 	})
 	port := 3000
-	fmt.Println("Server is running on port:", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	cert := "cert.pem"
+	key := "key.pem"
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS11,
+	}
+	server := &http.Server{
+		Addr:      fmt.Sprintf(":%d", port),
+		Handler:   nil,
+		TLSConfig: tlsConfig,
+	}
+
+	//Enable http2
+	http2.ConfigureServer(server, &http2.Server{})
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatalln("Could not start server", err)
 	}
+	//HTTP 1
+	// fmt.Println("Server is running on port:", port)
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
 }
