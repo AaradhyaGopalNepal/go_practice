3dadedfe4e19e7ddc432caf0b6bfde57c8cf6538
Aaradhya Gopal Nepal <aradhya.1221@gmail.com>
2026-03-25 21:17:29 +0545

    Implementing grpc addition

--- gRPC/server.go
+++ gRPC/server.go
@@ -1,11 +1,24 @@
-package grpc
+package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "gRPC/proto/gen"
+
 	"google.golang.org/grpc"
 )
+
+type server struct {
+	pb.UnimplementedCalculateServer
+}
+
+func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
+	return &pb.AddResponse{
+		Sum: req.A + req.B,
+	}, nil
+}
 
 func main() {
 	port := ":50051"
@@ -14,6 +27,7 @@ func main() {
 		log.Fatal("Failed to listen:", err)
 	}
 	grpcServer := grpc.NewServer()
+	pb.RegisterCalculateServer(grpcServer, &server{})
 
 	log.Println("Server is running on port", port)
 	err = grpcServer.Serve(lis)
